d4a894962e0364243f04e6293583953ad1c94774
Jesus Martin <jesustech88@gmail.com>
2020-12-16 15:34:09 +0000

    Reverting mongodb connection

--- src/main/java/com/example/secret_retro/dao/SecretRetroDao.java
+++ src/main/java/com/example/secret_retro/dao/SecretRetroDao.java
@@ -2,17 +2,10 @@ package com.example.secret_retro.dao;
 
 import com.example.secret_retro.model.BubbleData;
 import com.example.secret_retro.model.LabelType;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.Block;
-import com.mongodb.client.*;
 import lombok.extern.slf4j.Slf4j;
-import org.bson.Document;
-import org.bson.conversions.Bson;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,50 +13,11 @@ import java.util.List;
 @Repository
 public class SecretRetroDao implements ISecretRetroDao {
 
-    private static final String QUERY_AVERAGE = "[\n" +
-            "  {\n" +
-            "    \"$match\": {\n" +
-            "      \"created_at\": {\n" +
-            "        \"$nin\": [\n" +
-            "          null,\n" +
-            "          \"\"\n" +
-            "        ]\n" +
-            "      }\n" +
-            "    }\n" +
-            "  },\n" +
-            "  {\n" +
-            "    \"$group\": {\n" +
-            "      \"_id\": {},\n" +
-            "      \"__alias_0\": {\n" +
-            "        \"$avg\": \"$rating\"\n" +
-            "      }\n" +
-            "    }\n" +
-            "  },\n" +
-            "  {\n" +
-            "    \"$project\": {\n" +
-            "      \"_id\": 0,\n" +
-            "      \"__alias_0\": 1\n" +
-            "    }\n" +
-            "  },\n" +
-            "  {\n" +
-            "    \"$project\": {\n" +
-            "      \"average_rating\": \"$__alias_0\",\n" +
-            "      \"_id\": 0\n" +
-            "    }\n" +
-            "  },\n" +
-            "  {\n" +
-            "    \"$limit\": 5000\n" +
-            "  }\n" +
-            "]";
-    private MongoDatabase database;
-    private MongoCollection feedbacksCollection;
-
     SecretRetroDao(){
-        MongoClient mongoClient = MongoClients.create(
-                "mongodb+srv://secret-retro:secret-retro@cluster0.cstc5.mongodb.net/secretretro?retryWrites=true&w=majority");
-        database = mongoClient.getDatabase("secretretro");
-        feedbacksCollection = database.getCollection("feedbacks");
+
     }
+
+
 
 
     @Override
@@ -88,22 +42,7 @@ public class SecretRetroDao implements ISecretRetroDao {
         BubbleData b1 = BubbleData.builder().label("wfh").type(LabelType.BAD.toString()).value(10).build();
         BubbleData b2 = BubbleData.builder().label("spring planning").type(LabelType.GOOD.toString()).value(3).build();
        BubbleData b3 = BubbleData.builder().label("P1 bugfix").type(LabelType.GOOD.toString()).value(2).build();
-        log.info("There are " + feedbacksCollection.countDocuments()+ " documents ");
-        List<Bson> averageMetricQuery = null;
-        try {
-            averageMetricQuery = (List<Bson>) new ObjectMapper().readValue(QUERY_AVERAGE, List.class);
-        } catch (JsonProcessingException e) {
-            log.error(e.getMessage(), e);
-        }
 
-        List<Integer> averageRating = new ArrayList<>();
-        Block<Document> block = new Block<Document>() {
-            public void apply(Document document) {
-                averageRating.add(document.getInteger("average_rating"));
-            }
-        };
-        //database.getCollection("feedbacks").aggregate(averageMetricQuery).forEach(block);
-        //log.info("Average is "+averageRating.get(0));
         return Arrays.asList(b1, b2, b3);
     }
 }
