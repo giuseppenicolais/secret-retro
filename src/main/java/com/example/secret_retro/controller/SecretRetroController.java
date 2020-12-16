1e0db5e279934e738120818c27592531be79208b
Giuseppe Nicolais Russo <giuseppe.russo@clavistechnology.com>
2020-12-16 14:10:14 +0000

    adding cors

--- src/main/java/com/example/secret_retro/controller/SecretRetroController.java
+++ src/main/java/com/example/secret_retro/controller/SecretRetroController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,7 @@ import javax.validation.Valid;
 @RequestMapping(value = "/secret_retro", produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 @Validated
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SecretRetroController implements ISecretRetroController {
 
     @Autowired
